e07ea914e50cceac434f0012c3d2dcbcbfab3944
程若前 <evachengrq@hodowakamaes-MacBook-Air.local>
2020-09-25 16:37:12 +0800

    [evacheng]feat: implemented product

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header'
+import Product from './Components/Product'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Product />
     </div>
   );
 }
--- front-end/src/Components/Product.js
+++ front-end/src/Components/Product.js
@@ -1,44 +1,18 @@
 import React, { Component } from 'react';
-import pic from '.../logo.svg'
+import './Product.css'
+import pic from '../logo.svg';
 
 class Product extends Component {
-    state = {
-        products : [ 
-            {
-                product: '可乐1',
-                price: '1元/瓶'
-            },
-            {
-                product: '可乐2',
-                price: '1元/瓶'
-            },
-            {
-                product: '可乐3',
-                price: '1元/瓶'
-            },
-            {
-                product: '可乐4',
-                price: '1元/瓶'
-            },
-            {
-                product: '可乐5',
-                price: '1元/瓶'
-            },
-            {
-                product: '可乐6',
-                price: '1元/瓶'
-            },
-        ],
-        count: 0,
-    }
-
     render() {
         return (
-            <div>
-                <img src = {pic} />
-                <strong></strong>
+            <div className = "product">
+                <img src = {pic}></img>
+                <p className = "productName">可乐1</p>
+                <p className = "productPrice">单价：一元/瓶</p>
+                <button> + </button>
             </div>
         )
-
     }
 }
+
+export default Product;
